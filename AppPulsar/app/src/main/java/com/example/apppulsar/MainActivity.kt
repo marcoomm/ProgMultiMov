537f5a590752180c1489194097e89d3d8f26637b
dsoboy <4g63fp@gmail.com>
2024-10-29 14:11:58 +0100

    up-sss

---
+++ AppPulsar/app/src/main/java/com/example/apppulsar/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.apppulsar
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.ExtraBold
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.apppulsar.ui.theme.AppPulsarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppPulsarTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MiApp(){
+    Surface(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.primary)
+    {
+        Column(modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Text(text = "100€", style = TextStyle(
+                color = Color.White,
+                fontSize = 35.sp,
+                fontWeight = ExtraBold
+            ))
+            Spacer(modifier = Modifier.height(130.dp))
+            CrearCirculo()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CrearCirculo(){
+    var acumuladorDinero = 0
+    Card(modifier = Modifier
+        .padding(10.dp)
+        .size(105.dp)
+        .clickable {
+            acumuladorDinero +=1
+            Log.d("Acumulador $acumuladorDinero", "contador:  $acumuladorDinero")
+        },
+        shape = CircleShape
+    )
+    {
+        Column (modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Text(text="Pulsar $acumuladorDinero", modifier = Modifier)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VistaPreeliminar(){
+    AppPulsarTheme {
+        MiApp()
+    }
+}
---
+++ appTarjetaVisita/app/src/main/java/com/example/apptarjetavisita/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.apptarjetavisita
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apptarjetavisita.ui.theme.AppTarjetaVisitaTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTarjetaVisitaTheme {
+                Surface(color = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.primary
+                ) {
+                    crearTarjetaVisita()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun crearTarjetaVisita() {
+    val estadoDelBoton=remember{
+        mutableStateOf(false)
+    }
+    Surface(modifier=Modifier.fillMaxWidth().fillMaxSize()){
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(30.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Red,
+            ),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ){
+            Column(modifier = Modifier.height(300.dp).width(500.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                fotoPerfil()
+                HorizontalDivider(thickness = 5.dp)
+                infoPerfil()
+                Button(
+                    onClick={
+                        estadoDelBoton.value=!estadoDelBoton.value
+                    }
+                ){
+                    Text("Portfolio", style = MaterialTheme.typography.labelLarge)
+                }
+            }
+            if(estadoDelBoton.value){
+                Contenido()
+            }else{
+                Box()
+            }
+        }
+            }
+}
+
+fun Box() {
+}
+
+@Preview
+@Composable
+fun Contenido(){
+    Box(modifier = Modifier.fillMaxHeight().fillMaxSize().padding(5.dp)){
+        Surface(modifier = Modifier.padding(3.dp).fillMaxHeight().fillMaxSize(),
+            shape = RoundedCornerShape(corner= CornerSize(6.dp)),
+            border= BorderStroke(width = 2.dp, color = Color.Gray)
+        ){
+            Portfolio(data= listOf("Proyecto 1","Proyecto 2","Proyecto 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data){
+            item->Text(item)
+        }
+    }
+}
+
+
+
+@Composable
+private fun fotoPerfil(modifier: Modifier=Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foto2),
+            contentDescription = "Imagen de perfil",
+            modifier = Modifier.size(135.dp)
+        )
+    }
+}
+
+@Composable
+private fun infoPerfil(){
+    Column(modifier = Modifier.padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text="Perico el de los palotes",
+            style=MaterialTheme.typography.headlineMedium,
+            color=MaterialTheme.colorScheme.primary)
+        Text(text="Programador android",
+            modifier = Modifier.padding(3.dp))
+        Text(text="@developGuay",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.labelLarge)
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppTarjetaVisitaTheme{
+        crearTarjetaVisita()
+    }
+
+
+
+}
